2ffb99fc2db3697c054898452eeb0498a1e38693
Paschal Onor <paschalonor@gmail.com>
2025-06-14 21:12:14 -0400

    another commit

--- aws/cicd/terraform.tf
+++ aws/cicd/terraform.tf
@@ -7,7 +7,7 @@ terraform {
 
   backend "s3" {
     bucket         = "terraform-backend-terraformbbackends3bucket-ynqax1cfz9ow"
-    key            = "Development"
+    key            = "Development1"
     region         = "us-east-1"
     dynamodb_table = "terraform-backend-TerraformBackendDynamoDBTable-1MKII9GK8CJ11"
   }
